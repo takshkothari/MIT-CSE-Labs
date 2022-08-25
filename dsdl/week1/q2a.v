ffb8a988091cf0feb3c20c983b96ec41805f770c
takshkothari <96124613+takshkothari@users.noreply.github.com>
2022-08-25 08:49:15 +0530

    Delete dsdl/week1 directory

--- dsdl/week1/q2a.v
+++
@@ -1,7 +0,0 @@
-module q2a(a, b, c, d, f, g);
-   input a, b, c, d;
-   output f, g;
-   nand (x, a, b);
-   xor (f, d, x, c);
-   nor (g, b, c, d);
-endmodule   
--- dsdl/week1/q2b.v
+++
@@ -1,9 +0,0 @@
-module q2b(a, b, c, d, f, g);
-   input a, b, c, d;
-   output f, g;
-   wire x, y;
-   assign x = a&b;
-   assign f = ~x^c^d;
-   assign y = b|c|d;
-   assign g = ~y;
-endmodule
