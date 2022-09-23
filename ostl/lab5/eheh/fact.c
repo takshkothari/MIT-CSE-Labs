fbbc8a2028815e28b77c1c370cf4e2a60c2fd3f9
Taksh Kothari <96124613+takshkothari@users.noreply.github.com>
2022-09-23 14:59:24 +0530

    Add files via upload

---
+++ ostl/lab5/eheh/fact.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int num;
+	do{
+		printf("Enter a positive integer");
+		scanf("%d",&num);
+	} while(num<0);
+	
+	int factorial, i;
+	for (i = 1; i<=num; i++){
+		factorial = factorial *i;
+	}
+	printf("%d! = %d\n", num, factorial);
+	return 0;
+}
---
+++ ostl/lab5/helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main() {
+
+	int n;
+	printf("enter a number ");
+	scanf("%d",&n);
+	printf("nice lol\n");
+}
