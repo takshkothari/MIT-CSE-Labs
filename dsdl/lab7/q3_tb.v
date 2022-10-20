deefb1f5197c0fb1a1545489b7bcc34319575b3b
Taksh Kothari <96124613+takshkothari@users.noreply.github.com>
2022-10-20 09:50:36 +0530

    Add files via upload

--- dsdl/lab7/q3_tb.v
+++ dsdl/lab7/q3_tb.v
@@ -8,20 +8,20 @@ wire [3:0] x;
 q3 q3(w,x);
 initial
 begin 
-    $dumpfile("q3_tb.vcd");
-    $dumpvars(0,q3_tb);
+	$dumpfile("q3_tb.vcd");
+	$dumpvars(0,q3_tb);
 
-    w = 4'b0000; #20;
-    w = 4'b0001; #20;
-    w = 4'b0010; #20;
-    w = 4'b0011; #20;
-    w = 4'b0100; #20;
-    w = 4'b0101; #20;
-    w = 4'b0110; #20;
-    w = 4'b0111; #20;
-    w = 4'b1000; #20;
-    w = 4'b1001; #20;
+	w = 4'b0000; #20;
+	w = 4'b0001; #20;
+	w = 4'b0010; #20;
+	w = 4'b0011; #20;
+	w = 4'b0100; #20;
+	w = 4'b0101; #20;
+	w = 4'b0110; #20;
+	w = 4'b0111; #20;
+	w = 4'b1000; #20;
+	w = 4'b1001; #20;
 
-    $display("Test Complete");
+	$display("Test Complete");
 end
 endmodule
