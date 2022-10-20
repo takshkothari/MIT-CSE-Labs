4d8b35548c49db2c53e9d918a939b9c944b5714d
Taksh Kothari <96124613+takshkothari@users.noreply.github.com>
2022-10-20 11:22:16 +0530

    Add files via upload

--- dsdl/lab8/q3.v
+++ dsdl/lab8/q3.v
@@ -4,14 +4,14 @@ module d24(w,e,y);
 	output reg [0:3] y;
 	always @(w or e)
 	begin
-		y=4'b0000;
+		y=4'b1111;
 		if (e == 1)
 		begin
 			case(w)
-				0:  y[0] = 1;
-				1:  y[1] = 1;
-				2:  y[2] = 1;
-				3:  y[3] = 1; 
+				0:  y[0] = 0;
+				1:  y[1] = 0;
+				2:  y[2] = 0;
+				3:  y[3] = 0; 
 			endcase
 		end
 	end
@@ -24,9 +24,9 @@ module q3(w,e,y);
 	wire [0:3] y2;
     
 	d24 stage0(w[3:2],e,y2);
-	d24 stage1(w[1:0],y2[0],y[0:3]);
-	d24 stage2(w[1:0],y2[1],y[4:7]);
-	d24 stage3(w[1:0],y2[2],y[8:11]);
-	d24 stage4(w[1:0],y2[3],y[12:15]);
+	d24 stage1(w[1:0],~y2[0],y[0:3]);
+	d24 stage2(w[1:0],~y2[1],y[4:7]);
+	d24 stage3(w[1:0],~y2[2],y[8:11]);
+	d24 stage4(w[1:0],~y2[3],y[12:15]);
 
 endmodule
