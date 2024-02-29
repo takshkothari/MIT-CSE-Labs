8886168361c9407187b285b9ebb16e46d38950aa
Taksh Kothari <96124613+takshkothari@users.noreply.github.com>
2024-02-29 11:03:55 +0530

    bully fixed

--- sem-6-labs/dsl/lab8/bully.py
+++ sem-6-labs/dsl/lab8/bully.py
@@ -1,25 +1,27 @@
 import sys
 
 noOfNodes = int(sys.argv[1])
+failedNodes = noOfNodes
 initiatorNode = int(sys.argv[2])
 
 def bully_algorithm():
     print("BULLY ALGORITHM SIMULATION:")
     print('Node %s notices the current coordinator %s has failed' % (initiatorNode, noOfNodes))
     biggerNodes = []
-    for i in range(initiatorNode+1, noOfNodes):
+    for i in range(initiatorNode+1, noOfNodes+1):
         print("%s sends ELECTION message to %s" % (initiatorNode,i))
-        biggerNodes.append(i)
+        if i != failedNodes:
+            biggerNodes.append(i)
     for i in biggerNodes:
         print("%s sends OK message to %s" % (i, initiatorNode))
-    while len(biggerNodes) != 1:
+    while len(biggerNodes) > 0:
         i = biggerNodes[0]
-        for j in range(i+1, noOfNodes):
+        for j in range(i+1, noOfNodes+1):
             print("%s sends ELECTION message to %s" % (i, j))
         for k in range(i+1, noOfNodes):
             print("%s sends OK message to %s" % (k, i))
         biggerNodes.remove(i)
-    newCoordinatorNode = biggerNodes[0]
+    newCoordinatorNode = i
     for i in range(0, newCoordinatorNode):
         print("%s sends COORDINATOR message to %s" %(newCoordinatorNode, i))
 
